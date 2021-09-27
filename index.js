73d1bf36755be2ab4184b59834ac5e2d90d12e5f
FAL <tobias.falkner@combeenation.com>
2021-09-27 15:36:26 +0200

    adjust line-breaks to always match it's source

--- index.js
+++ index.js
@@ -2,13 +2,50 @@
 
 const cp = require('child_process');
 const fs = require('fs');
+const path = require('path');
 
-const globInputFiles = './src/typings/*.hive.d.ts';
+const origFileExtension = '.hive.d.ts';
+const globInputFiles = './src/typings/*' + origFileExtension;
 const outDir = './src/typings-generated-objs';
 const generatedFileExtension = '.hive.d-ti.js';
 const progressMsg = 'Creating JS objects for TS interfaces...';
 
 class CreateITFObjectPlugin {
+  getLineBreakChar(str) {
+    const indexOfLF = str.indexOf('\n', 1); // No need to check first-character
+    if (indexOfLF === -1) {
+      if (str.indexOf('\r') !== -1) return '\r';
+      return '\n';
+    }
+
+    if (str[indexOfLF - 1] === '\r') return '\r\n';
+    return '\n';
+  }
+
+  /**
+   * Adjust the line endings of the auto-generated files if it doesn't match the source file
+   */
+  async adjustLineEndings() {
+    const generatedFiles = fs.readdirSync(outDir);
+    generatedFiles.forEach(async genFile => {
+      const genFilepath = path.join(outDir, genFile);
+      const srcFilepath = genFilepath
+        .replace('typings-generated-objs', 'typings')
+        .replace(generatedFileExtension, origFileExtension);
+
+      const srcFileContent = await fs.promises.readFile(srcFilepath, 'utf8');
+      const genFileContent = await fs.promises.readFile(genFilepath, 'utf8');
+      const srcEndChar = this.getLineBreakChar(srcFileContent);
+      const generatedEndChar = this.getLineBreakChar(genFileContent);
+
+      if (srcEndChar !== generatedEndChar) {
+        const regexReplace = new RegExp(generatedEndChar, 'g');
+        const newContent = genFileContent.replace(regexReplace, srcEndChar);
+        await fs.promises.writeFile(genFilepath, newContent);
+      }
+    });
+  }
+
   apply(compiler) {
     compiler.hooks.beforeCompile.tap('CreateITFObjectPlugin', () => {
       let onlyRuntimeTypingsChanged = false;
@@ -28,6 +65,7 @@ class CreateITFObjectPlugin {
 
         process.stderr.write(progressMsg);
         cp.execSync(`ts-interface-builder --format=js:esm --changed-only --outDir ${outDir} ${globInputFiles}`);
+        this.adjustLineEndings();
         // remove previous message with backspaces (\b)
         process.stderr.write('\b'.repeat(progressMsg.length));
       }
